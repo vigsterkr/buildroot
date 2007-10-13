09ffb3065ca7415fc9668e93860d9b604b94f388
Bernhard Fischer <rep.dot.nop@gmail.com>
2007-10-13 23:33:59 +0200

    make sure that we really cross-compile the wipe binary
      This could need a little love to touch it up a bit, code-wise..

--- package/wipe/wipe.mk
+++ package/wipe/wipe.mk
@@ -2,7 +2,6 @@
 #
 # wipe
 #
-# http://abaababa.ouvaton.org/wipe/wipe-$(WIPE_VERSION).tar.gz
 #############################################################
 WIPE_VERSION:=0.20
 WIPE_SOURCE:=wipe-$(WIPE_VERSION).tar.gz
@@ -13,6 +12,11 @@ WIPE_DIR:=$(BUILD_DIR)/wipe-$(WIPE_VERSION)
 WIPE_BINARY:=wipe
 WIPE_TARGET_BINARY:=bin/wipe
 
+WIPE_CFLAGS:=$(TARGET_CFLAGS)
+ifeq ($(BR2_LARGEFILE),y)
+WIPE_CFLAGS+=-D_LARGEFILE_SOURCE -D_FILE_OFFSET_BITS=64
+endif
+
 $(DL_DIR)/$(WIPE_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(WIPE_SITE)/$(WIPE_SOURCE)
 
@@ -21,28 +25,30 @@ $(DL_DIR)/$(WIPE_PATCH):
 	 $(WGET) -P $(DL_DIR) $(WIPE_SITE)/$(WIPE_PATCH)
 endif
 
-wipe-source: $(DL_DIR)/$(WIPE_SOURCE) $(DL_DIR)/$(WIPE_PATCH)
-
 $(WIPE_DIR)/.unpacked: $(DL_DIR)/$(WIPE_SOURCE) $(DL_DIR)/$(WIPE_PATCH)
 	$(WIPE_CAT) $(DL_DIR)/$(WIPE_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	#toolchain/patch-kernel.sh $(WIPE_DIR) $(DL_DIR) $(WIPE_PATCH)
-	touch $(WIPE_DIR)/.unpacked
-
-$(WIPE_DIR)/.configured: $(WIPE_DIR)/.unpacked
 	touch $@
 
-$(WIPE_DIR)/$(WIPE_BINARY): $(WIPE_DIR)/.configured
+$(WIPE_DIR)/$(WIPE_BINARY): $(WIPE_DIR)/.unpacked
 	rm -f $(WIPE_DIR)/$(WIPE_BINARY)
-	$(MAKE) CC=$(TARGET_CC) -C $(WIPE_DIR) generic
+	$(MAKE) CC_GENERIC="$(TARGET_CC) $(WIPE_CFLAGS)" \
+		CCO_GENERIC="$(WIPE_FLAGS)" \
+		CCOC_GENERIC=-c -C $(WIPE_DIR) generic
+	touch -c $@
 
 $(TARGET_DIR)/$(WIPE_TARGET_BINARY): $(WIPE_DIR)/$(WIPE_BINARY)
-	cp -a $(WIPE_DIR)/$(WIPE_BINARY) $(TARGET_DIR)/$(WIPE_TARGET_BINARY)
+	$(INSTALL) -D $(WIPE_DIR)/$(WIPE_BINARY) $(TARGET_DIR)/$(WIPE_TARGET_BINARY)
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $@
 
 wipe: uclibc $(TARGET_DIR)/$(WIPE_TARGET_BINARY)
+
+wipe-source: $(DL_DIR)/$(WIPE_SOURCE) $(DL_DIR)/$(WIPE_PATCH)
 
 wipe-clean:
 	#$(MAKE) DESTDIR=$(TARGET_DIR) CC=$(TARGET_CC) -C $(WIPE_DIR) uninstall
 	-$(MAKE) -C $(WIPE_DIR) clean
+	rm -f $(TARGET_DIR)/$(WIPE_TARGET_BINARY)
 
 wipe-dirclean:
 	rm -rf $(WIPE_DIR)
