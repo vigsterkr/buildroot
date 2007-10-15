6eb765a16808dfde8061c386771eb18713348160
Bernhard Fischer <rep.dot.nop@gmail.com>
2007-10-15 20:29:43 +0200

    bump version of pciutils
      Minor cleanups while at it.

--- package/pciutils/pciutils.mk
+++ package/pciutils/pciutils.mk
@@ -3,7 +3,7 @@
 # pciutils
 #
 #############################################################
-PCIUTILS_VERSION:=2.2.6
+PCIUTILS_VERSION:=2.2.7
 PCIUTILS_SOURCE:=pciutils-$(PCIUTILS_VERSION).tar.gz
 PCIUTILS_CAT:=$(ZCAT)
 PCIUTILS_SITE:=ftp://atrey.karlin.mff.cuni.cz/pub/linux/pci
@@ -20,34 +20,38 @@ $(DL_DIR)/$(PCIUTILS_SOURCE):
 $(DL_DIR)/$(PCIIDS_SOURCE):
 	$(WGET) -P $(DL_DIR) $(PCIIDS_SITE)/$(PCIIDS_SOURCE)
 
-pciutils-source: $(DL_DIR)/$(PCIUTILS_SOURCE) $(DL_DIR)/$(PCIIDS_SOURCE)
-
 $(PCIUTILS_DIR)/.unpacked: $(DL_DIR)/$(PCIUTILS_SOURCE) $(DL_DIR)/$(PCIIDS_SOURCE)
 	$(PCIUTILS_CAT) $(DL_DIR)/$(PCIUTILS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	$(PCIIDS_CAT) $(DL_DIR)/$(PCIIDS_SOURCE) > $(PCIUTILS_DIR)/pci.ids
 	toolchain/patch-kernel.sh $(PCIUTILS_DIR) package/pciutils pciutils\*.patch
-	touch $(PCIUTILS_DIR)/.unpacked
+	$(CONFIG_UPDATE) $(@D)
+	touch $@
 
 $(PCIUTILS_DIR)/.compiled: $(PCIUTILS_DIR)/.unpacked
-	$(MAKE1) CC=$(TARGET_CC) OPT="$(TARGET_CFLAGS)" -C $(PCIUTILS_DIR) \
+	$(MAKE1) CC="$(TARGET_CC)" OPT="$(TARGET_CFLAGS)" -C $(PCIUTILS_DIR) \
 		SHAREDIR="/usr/share/misc" \
 		PREFIX=/usr
-	touch $(PCIUTILS_DIR)/.compiled
+	touch $@
 
 $(TARGET_DIR)/sbin/lspci: $(PCIUTILS_DIR)/.compiled
-	install -c $(PCIUTILS_DIR)/lspci $(TARGET_DIR)/sbin/lspci
+	$(INSTALL) $(PCIUTILS_DIR)/lspci $(TARGET_DIR)/sbin/lspci
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $@
 
 $(TARGET_DIR)/sbin/setpci: $(PCIUTILS_DIR)/.compiled
-	install -c $(PCIUTILS_DIR)/setpci $(TARGET_DIR)/sbin/setpci
+	$(INSTALL) $(PCIUTILS_DIR)/setpci $(TARGET_DIR)/sbin/setpci
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $@
 
 $(TARGET_DIR)/usr/share/misc/pci.ids: $(PCIUTILS_DIR)/.unpacked
-	install -Dc $(PCIUTILS_DIR)/pci.ids $(TARGET_DIR)/usr/share/misc/pci.ids
+	$(INSTALL) -D $(PCIUTILS_DIR)/pci.ids $(TARGET_DIR)/usr/share/misc/pci.ids
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $@
 
 pciutils: uclibc $(TARGET_DIR)/sbin/setpci $(TARGET_DIR)/sbin/lspci $(TARGET_DIR)/usr/share/misc/pci.ids
 
+pciutils-source: $(DL_DIR)/$(PCIUTILS_SOURCE) $(DL_DIR)/$(PCIIDS_SOURCE)
+
 pciutils-clean:
-	rm $(TARGET_DIR)/sbin/lspci $(TARGET_DIR)/sbin/setpci $(TARGET_DIR)/usr/share/misc/pci.ids
 	-$(MAKE) -C $(PCIUTILS_DIR) clean
+	rm $(TARGET_DIR)/sbin/lspci $(TARGET_DIR)/sbin/setpci $(TARGET_DIR)/usr/share/misc/pci.ids
 
 pciutils-dirclean:
 	rm -rf $(PCIUTILS_DIR)
