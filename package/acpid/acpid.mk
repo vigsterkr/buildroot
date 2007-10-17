21461dc51ff798c6adade620ffbe3f5c602c1982
Bernhard Fischer <rep.dot.nop@gmail.com>
2007-10-17 16:13:49 +0200

    bump version of acpid

--- package/acpid/acpid.mk
+++ package/acpid/acpid.mk
@@ -3,37 +3,48 @@
 # acpid
 #
 #############################################################
-ACPID_VERSION:=1.0.4
+ACPID_VERSION:=1.0.6
 ACPID_DIR=$(BUILD_DIR)/acpid-$(ACPID_VERSION)
-ACPID_SOURCE=acpid_$(ACPID_VERSION)-1.tar.gz
+ACPID_SOURCE=acpid_$(ACPID_VERSION).orig.tar.gz
+ACPID_PATCH=acpid_$(ACPID_VERSION)-4.diff.gz
 ACPID_SITE=http://ftp.debian.org/debian/pool/main/a/acpid
+ACPID_CAT=$(ZCAT)
 
 $(DL_DIR)/$(ACPID_SOURCE):
 	$(WGET) -P $(DL_DIR) $(ACPID_SITE)/$(ACPID_SOURCE)
 
-$(ACPID_DIR)/.unpacked: $(DL_DIR)/$(ACPID_SOURCE)
-	$(ZCAT) $(DL_DIR)/$(ACPID_SOURCE) | tar -C $(BUILD_DIR) -xvf -
+ifneq ($(ACPID_PATCH),)
+ACPID_PATCH_FILE=$(DL_DIR)/$(ACPID_PATCH)
+$(ACPID_PATCH_FILE):
+	$(WGET) -P $(DL_DIR) $(ACPID_SITE)/$(ACPID_PATCH)
+endif
+
+$(ACPID_DIR)/.unpacked: $(DL_DIR)/$(ACPID_SOURCE) $(ACPID_PATCH_FILE)
+	$(ACPID_CAT) $(DL_DIR)/$(ACPID_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
+ifneq ($(ACPID_PATCH),)
+	(cd $(ACPID_DIR) && $(ACPID_CAT) $(ACPID_PATCH_FILE) | patch -p1)
+endif
 	toolchain/patch-kernel.sh $(ACPID_DIR) package/acpid/ acpid\*.patch
-	touch $(ACPID_DIR)/.unpacked
+	touch $@
 
 $(ACPID_DIR)/acpid: $(ACPID_DIR)/.unpacked
-	$(MAKE) CC=$(TARGET_CC) -C $(ACPID_DIR)
-	$(STRIPCMD) $(STRIP_STRIP_ALL) $(ACPID_DIR)/acpid
-	$(STRIPCMD) $(STRIP_STRIP_ALL) $(ACPID_DIR)/acpi_listen
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(ACPID_DIR)
 	touch -c $(ACPID_DIR)/acpid $(ACPID_DIR)/acpi_listen
 
 $(TARGET_DIR)/usr/sbin/acpid: $(ACPID_DIR)/acpid
-	cp -a $(ACPID_DIR)/acpid $(TARGET_DIR)/usr/sbin/acpid
-	mkdir -p $(TARGET_DIR)/etc/acpi/events
+	$(INSTALL) -D $(ACPID_DIR)/acpid $(TARGET_DIR)/usr/sbin/acpid
+	$(INSTALL) -d $(TARGET_DIR)/etc/acpi/events
 	/bin/echo -e "event=button[ /]power\naction=/sbin/poweroff" > $(TARGET_DIR)/etc/acpi/events/powerbtn
-	touch -c $(TARGET_DIR)/usr/sbin/acpid
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $@
 
 acpid: $(TARGET_DIR)/usr/sbin/acpid
 
-acpid-source: $(DL_DIR)/$(ACPID_SOURCE)
+acpid-source: $(DL_DIR)/$(ACPID_SOURCE) $(ACPID_PATCH_FILE)
 
 acpid-clean:
 	-make -C $(ACPID_DIR) clean
+	rm -rf $(TARGET_DIR)/usr/sbin/acpid \
+		$(TARGET_DIR)/etc/acpi
 
 acpid-dirclean:
 	rm -rf $(ACPID_DIR)
