9960ea4c70b00a96fb92471ff72a175ac10c1ab4
Bernhard Fischer <rep.dot.nop@gmail.com>
2007-10-19 14:36:58 +0200

    olsrd belongs into usr/sbin/ rather than usr/bin
      Cleanup while at it

--- package/olsr/olsr.mk
+++ package/olsr/olsr.mk
@@ -12,7 +12,7 @@ OLSR_SITE:=http://www.olsr.org/releases/$(OLSR_VERSION_MAJOR)
 OLSR_DIR:=$(BUILD_DIR)/olsrd-$(OLSR_VERSION)
 OLSR_CAT:=$(BZCAT)
 OLSR_BINARY:=olsrd
-OLSR_TARGET_BINARY:=usr/bin/olsrd
+OLSR_TARGET_BINARY:=usr/sbin/olsrd
 #OLSR_PLUGINS=httpinfo tas dot_draw nameservice dyn_gw dyn_gw_plain pgraph bmf quagga secure
 OLSR_PLUGINS=dot_draw dyn_gw secure
 OLSR_TARGET_PLUGIN=usr/lib/
@@ -20,35 +20,34 @@ OLSR_TARGET_PLUGIN=usr/lib/
 $(DL_DIR)/$(OLSR_SOURCE):
 	$(WGET) -P $(DL_DIR) $(OLSR_SITE)/$(OLSR_SOURCE)
 
-olsr-source: $(DL_DIR)/$(OLSR_SOURCE)
-
-olsr-unpacked: $(OLSR_DIR)/.unpacked
 $(OLSR_DIR)/.unpacked: $(DL_DIR)/$(OLSR_SOURCE)
 	$(OLSR_CAT) $(DL_DIR)/$(OLSR_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	touch $@
 
 $(OLSR_DIR)/$(OLSR_BINARY): $(OLSR_DIR)/.unpacked
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(OLSR_DIR) olsrd $(OLSR_PLUGINS)
+	touch -c $@
 
 $(TARGET_DIR)/$(OLSR_TARGET_BINARY): $(OLSR_DIR)/$(OLSR_BINARY)
 	rm -f $(TARGET_DIR)/$(OLSR_TARGET_BINARY)
-	cp -dpf $(OLSR_DIR)/$(OLSR_BINARY) $(TARGET_DIR)/$(OLSR_TARGET_BINARY)
+	$(INSTALL) -D -m 0755 $(OLSR_DIR)/$(OLSR_BINARY) $(TARGET_DIR)/$(OLSR_TARGET_BINARY)
 	cp -R $(OLSR_DIR)/lib/*/olsrd_*.so* $(TARGET_DIR)/$(OLSR_TARGET_PLUGIN)
-	mkdir -p $(TARGET_DIR)/etc/init.d
-	cp -dpf package/olsr/S50olsr $(TARGET_DIR)/etc/init.d/
+	$(INSTALL) -D -m 0755 package/olsr/S50olsr $(TARGET_DIR)/etc/init.d/S50olsr
 	test -r $(TARGET_DIR)/etc/olsrd.conf || \
-		cp -dpf $(OLSR_DIR)/files/olsrd.conf.default.lq $(TARGET_DIR)/etc/olsrd.conf
+		$(INSTALL) -D -m 0644 $(OLSR_DIR)/files/olsrd.conf.default.lq $(TARGET_DIR)/etc/olsrd.conf
 	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/$(OLSR_TARGET_PLUGIN)/olsrd_*.so*
 	$(STRIPCMD) $(STRIP_STRIP_ALL) $@
 
 olsr: uclibc $(TARGET_DIR)/$(OLSR_TARGET_BINARY)
 
+olsr-source: $(DL_DIR)/$(OLSR_SOURCE)
+
 olsr-clean:
+	-$(MAKE) -C $(OLSR_DIR) clean
 	rm -f $(TARGET_DIR)/$(OLSR_TARGET_BINARY) \
 		$(TARGET_DIR)/$(OLSR_TARGET_PLUGIN)/olsrd_*.so* \
 		$(TARGET_DIR)/etc/init.d/S50olsr \
 		$(TARGET_DIR)/etc/olsrd.conf
-	-$(MAKE) -C $(OLSR_DIR) clean
 
 olsr-dirclean:
 	rm -rf $(OLSR_DIR)
@@ -58,6 +57,6 @@ olsr-dirclean:
 # Toplevel Makefile options
 #
 #############################################################
-ifeq ($(strip $(BR2_PACKAGE_OLSR)),y)
+ifeq ($(BR2_PACKAGE_OLSR),y)
 TARGETS+=olsr $(OLSR_PLUGINS)
 endif
