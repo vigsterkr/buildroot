422ee1e7f74f822c61ec7d1ebdac43158660e9cd
Bernhard Fischer <rep.dot.nop@gmail.com>
2007-10-19 21:15:33 +0200

    cleanup distcc

--- package/distcc/distcc.mk
+++ package/distcc/distcc.mk
@@ -14,50 +14,39 @@ DISTCC_TARGET_BINARY:=usr/bin/distcc
 $(DL_DIR)/$(DISTCC_SOURCE):
 	$(WGET) -P $(DL_DIR) $(DISTCC_SITE)/$(DISTCC_SOURCE)
 
-distcc-source: $(DL_DIR)/$(CVS_SOURCE)
-
 $(DISTCC_BUILDDIR)/.unpacked: $(DL_DIR)/$(DISTCC_SOURCE)
 	$(DISTCC_CAT) $(DL_DIR)/$(DISTCC_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	$(CONFIG_UPDATE) $(DISTCC_BUILDDIR)
-	touch $(DISTCC_BUILDDIR)/.unpacked
+	$(CONFIG_UPDATE) $(@D)
+	touch $@
 
 $(DISTCC_BUILDDIR)/.configured: $(DISTCC_BUILDDIR)/.unpacked
 	(cd $(DISTCC_BUILDDIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
+		$(AUTO_TARGET_CONFIGURE) \
 		--prefix=/usr \
-		--bindir=/usr/bin \
-		--sbindir=/usr/sbin \
-		--libdir=/lib \
-		--libexecdir=/usr/lib \
 		--sysconfdir=/etc \
-		--datadir=/usr/share \
-		--localstatedir=/var \
-		--mandir=/usr/man \
-		--infodir=/usr/info \
 		--with-included-popt \
 		--without-gtk \
 		--without-gnome \
 	)
-	touch $(DISTCC_BUILDDIR)/.configured
+	touch $@
 
 $(DISTCC_BUILDDIR)/$(DISTCC_BINARY): $(DISTCC_BUILDDIR)/.configured
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(DISTCC_BUILDDIR)
 
 $(TARGET_DIR)/$(DISTCC_TARGET_BINARY): $(DISTCC_BUILDDIR)/$(DISTCC_BINARY)
-	install -D $(DISTCC_BUILDDIR)/$(DISTCC_BINARY)d $(TARGET_DIR)/$(DISTCC_TARGET_BINARY)d
-	install -D $(DISTCC_BUILDDIR)/$(DISTCC_BINARY) $(TARGET_DIR)/$(DISTCC_TARGET_BINARY)
+	$(INSTALL) -D $(DISTCC_BUILDDIR)/$(DISTCC_BINARY)d $(TARGET_DIR)/$(DISTCC_TARGET_BINARY)d
+	$(INSTALL) -D $(DISTCC_BUILDDIR)/$(DISTCC_BINARY) $(TARGET_DIR)/$(DISTCC_TARGET_BINARY)
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $(TARGET_DIR)/$(DISTCC_TARGET_BINARY) \
+		$(TARGET_DIR)/$(DISTCC_TARGET_BINARY)d
 
 distcc: uclibc $(TARGET_DIR)/$(DISTCC_TARGET_BINARY)
 
+distcc-source: $(DL_DIR)/$(CVS_SOURCE)
+
 distcc-clean:
-	rm -f $(TARGET_DIR)/$(DISTCC_TARGET_BINARY)
-	rm -f $(TARGET_DIR)/$(DISTCC_TARGET_BINARY)d
 	-$(MAKE) -C $(DISTCC_BUILDDIR) clean
+	rm -f $(TARGET_DIR)/$(DISTCC_TARGET_BINARY) \
+		$(TARGET_DIR)/$(DISTCC_TARGET_BINARY)d
 
 distcc-dirclean:
 	rm -rf $(DISTCC_BUILDDIR)
