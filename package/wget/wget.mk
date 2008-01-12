96e9d48d5be2fbdcc0b5c4540de5444299b2f6bb
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-01-12 23:46:43 +0100

    honor DISABLE_IPV6

--- package/wget/wget.mk
+++ package/wget/wget.mk
@@ -14,12 +14,11 @@ WGET_TARGET_BINARY:=bin/wget
 $(DL_DIR)/$(WGET_SOURCE):
 	$(WGET) -P $(DL_DIR) $(WGET_SITE)/$(WGET_SOURCE)
 
-wget-source: $(DL_DIR)/$(WGET_SOURCE)
-
 $(WGET_DIR)/.unpacked: $(DL_DIR)/$(WGET_SOURCE)
 	$(WGET_CAT) $(DL_DIR)/$(WGET_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	touch $(WGET_DIR)/.unpacked
+	touch $@
 
+# TODO: conditional ssl support
 $(WGET_DIR)/.configured: $(WGET_DIR)/.unpacked
 	(cd $(WGET_DIR); rm -rf config.cache; \
 		$(TARGET_CONFIGURE_OPTS) \
@@ -29,19 +28,21 @@ $(WGET_DIR)/.configured: $(WGET_DIR)/.unpacked
 		--host=$(GNU_TARGET_NAME) \
 		--build=$(GNU_HOST_NAME) \
 		--prefix=/ \
-		--disable-ipv6 \
+		$(DISABLE_IPV6) \
 		$(DISABLE_NLS) \
 		--without-ssl \
 	)
-	touch $(WGET_DIR)/.configured
+	touch $@
 
 $(WGET_DIR)/$(WGET_BINARY): $(WGET_DIR)/.configured
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(WGET_DIR)
 
 $(TARGET_DIR)/$(WGET_TARGET_BINARY): $(WGET_DIR)/$(WGET_BINARY)
-	install -D $(WGET_DIR)/$(WGET_BINARY) $(TARGET_DIR)/$(WGET_TARGET_BINARY)
+	$(INSTALL) -D -m 0755 $(WGET_DIR)/$(WGET_BINARY) $(TARGET_DIR)/$(WGET_TARGET_BINARY)
 
 wget: uclibc $(TARGET_DIR)/$(WGET_TARGET_BINARY)
+
+wget-source: $(DL_DIR)/$(WGET_SOURCE)
 
 wget-clean:
 	rm -f $(TARGET_DIR)/$(WGET_TARGET_BINARY)
