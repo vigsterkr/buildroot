c6a069662f3ac9ff2a515338987db3706c6e8b74
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-01-21 14:40:01 +0100

    bump fakeroot version and cleanup package
      Correctness- and style fixes

--- package/fakeroot/fakeroot.mk
+++ package/fakeroot/fakeroot.mk
@@ -3,7 +3,7 @@
 # fakeroot
 #
 #############################################################
-FAKEROOT_VERSION:=1.8.10
+FAKEROOT_VERSION:=1.9
 FAKEROOT_SOURCE:=fakeroot_$(FAKEROOT_VERSION).tar.gz
 FAKEROOT_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/f/fakeroot
 FAKEROOT_CAT:=$(ZCAT)
@@ -15,14 +15,13 @@ FAKEROOT_DIR2:=$(BUILD_DIR)/fakeroot-$(FAKEROOT_VERSION)-target
 $(DL_DIR)/$(FAKEROOT_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(FAKEROOT_SITE)/$(FAKEROOT_SOURCE)
 
-fakeroot-source: $(DL_DIR)/$(FAKEROOT_SOURCE)
-
 $(FAKEROOT_SOURCE_DIR)/.unpacked: $(DL_DIR)/$(FAKEROOT_SOURCE)
 	$(FAKEROOT_CAT) $(DL_DIR)/$(FAKEROOT_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	# If using busybox getopt, make it be quiet.
 	$(SED) "s,getopt --version,getopt --version 2>/dev/null," \
 		$(FAKEROOT_SOURCE_DIR)/scripts/fakeroot.in
 	toolchain/patch-kernel.sh $(FAKEROOT_SOURCE_DIR) package/fakeroot/ \*.patch
+	$(CONFIG_UPDATE) $(@D)
 	touch $@
 
 
@@ -57,12 +56,13 @@ $(STAGING_DIR)/usr/bin/fakeroot: $(FAKEROOT_DIR1)/faked
 
 host-fakeroot: uclibc $(STAGING_DIR)/usr/bin/fakeroot
 
+host-fakeroot-source: $(DL_DIR)/$(FAKEROOT_SOURCE)
+
 host-fakeroot-clean:
-	$(MAKE) -C $(FAKEROOT_DIR1) clean
+	-$(MAKE) -C $(FAKEROOT_DIR1) clean
 
 host-fakeroot-dirclean:
-	rm -rf $(FAKEROOT_DIR1)
-
+	rm -rf $(FAKEROOT_DIR1) $(FAKEROOT_SOURCE_DIR)
 
 #############################################################
 #
@@ -70,15 +70,11 @@ host-fakeroot-dirclean:
 #
 #############################################################
 
+$(FAKEROOT_DIR2)/.configured: THIS_SRCDIR = $(FAKEROOT_SOURCE_DIR)
 $(FAKEROOT_DIR2)/.configured: $(FAKEROOT_SOURCE_DIR)/.unpacked
 	mkdir -p $(FAKEROOT_DIR2)
 	(cd $(FAKEROOT_DIR2); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		$(FAKEROOT_SOURCE_DIR)/configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
+		$(AUTO_CONFIGURE_TARGET) \
 		--prefix=/usr \
 		--exec-prefix=/usr \
 		--bindir=/usr/bin \
@@ -94,14 +90,14 @@ $(FAKEROOT_DIR2)/.configured: $(FAKEROOT_SOURCE_DIR)/.unpacked
 	touch $@
 
 $(FAKEROOT_DIR2)/faked: $(FAKEROOT_DIR2)/.configured
-	$(MAKE) CC=$(TARGET_CC) -C $(FAKEROOT_DIR2)
+	$(MAKE) -C $(FAKEROOT_DIR2)
 	touch -c $@
 
 $(TARGET_DIR)/usr/bin/fakeroot: $(FAKEROOT_DIR2)/faked
 	$(MAKE) DESTDIR=$(TARGET_DIR) -C $(FAKEROOT_DIR2) install
-	-mv $(TARGET_DIR)/usr/bin/$(ARCH)-linux-faked \
+	-mv $(TARGET_DIR)/usr/bin/$(REAL_GNU_TARGET_NAME)-faked \
 		$(TARGET_DIR)/usr/bin/faked
-	-mv $(TARGET_DIR)/usr/bin/$(ARCH)-linux-fakeroot \
+	-mv $(TARGET_DIR)/usr/bin/$(REAL_GNU_TARGET_NAME)-fakeroot \
 		$(TARGET_DIR)/usr/bin/fakeroot
 ifneq ($(BR2_HAVE_INFOPAGES),y)
 	rm -rf $(TARGET_DIR)/usr/share/info
@@ -111,17 +107,19 @@ ifneq ($(BR2_HAVE_MANPAGES),y)
 endif
 	rm -rf $(TARGET_DIR)/share/locale
 	rm -rf $(TARGET_DIR)/usr/share/doc
+	$(STRIPCMD) $(STRIP_STRIP_ALL) $(TARGET_DIR)/usr/bin/faked
 	touch -c $@
 
 fakeroot: uclibc $(TARGET_DIR)/usr/bin/fakeroot
 
+fakeroot-source: $(DL_DIR)/$(FAKEROOT_SOURCE)
+
 fakeroot-clean:
-	$(MAKE) -C $(FAKEROOT_DIR2) clean
-	rm -f $(TARGET_DIR)/usr/bin/fake{d,root}
+	-$(MAKE) -C $(FAKEROOT_DIR2) clean
+	rm -f $(TARGET_DIR)/usr/bin/faked $(TARGET_DIR)/usr/bin/fakeroot
 
 fakeroot-dirclean:
 	rm -rf $(FAKEROOT_DIR2)
-
 
 #############################################################
 #
@@ -131,7 +129,6 @@ fakeroot-dirclean:
 ifeq ($(BR2_PACKAGE_FAKEROOT),y)
 TARGETS+=fakeroot
 endif
-
 ifeq ($(BR2_HOST_FAKEROOT),y)
 HOST_SOURCE+=fakeroot-source
 endif
