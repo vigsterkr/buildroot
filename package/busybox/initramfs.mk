aef523b891a8d12e2cc70527b801c8df928f8205
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-01-24 10:51:22 +0100

    cosmetic cleanup

--- package/busybox/initramfs.mk
+++ package/busybox/initramfs.mk
@@ -8,6 +8,7 @@ ifeq ($(BR2_PACKAGE_BUSYBOX_INITRAMFS),y)
 BUSYBOX_INITRAMFS_DIR:=$(BUSYBOX_DIR)-initramfs
 BR2_INITRAMFS_DIR:=$(PROJECT_BUILD_DIR)/initramfs
 BB_INITRAMFS_TARGET:=$(IMAGE).initramfs_lst
+BB_INITRAMFS_TARGET_F:=$(notdir $(BB_INITRAMFS_TARGET))
 
 $(BUSYBOX_INITRAMFS_DIR)/.unpacked: $(DL_DIR)/$(BUSYBOX_SOURCE)
 	rm -rf $(BUILD_DIR)/tmp $(BUSYBOX_INITRAMFS_DIR)
@@ -117,15 +118,15 @@ $(BB_INITRAMFS_TARGET): host-fakeroot $(BR2_INITRAMFS_DIR)/bin/busybox
 	mkdir -p $(PROJECT_BUILD_DIR)/initramfs/etc
 	cat target/generic/target_busybox_skeleton/etc/inittab > \
 		$(PROJECT_BUILD_DIR)/initramfs/etc/inittab
-	rm -f $(PROJECT_BUILD_DIR)/_fakeroot.$(notdir $(BB_INITRAMFS_TARGET))
+	rm -f $(PROJECT_BUILD_DIR)/_fakeroot.$(BB_INITRAMFS_TARGET_F)
 	(echo "chown -R 0:0 $(PROJECT_BUILD_DIR)/initramfs"; \
 	 echo "$(STAGING_DIR)/bin/makedevs -d $(TARGET_DEVICE_TABLE) $(PROJECT_BUILD_DIR)/initramfs"; \
 	 echo "$(CONFIG_SHELL) target/initramfs/gen_initramfs_list.sh -u 0 -g 0 $(PROJECT_BUILD_DIR)/initramfs > $(BB_INITRAMFS_TARGET)"; \
-	) > $(PROJECT_BUILD_DIR)/_fakeroot.$(notdir $(BB_INITRAMFS_TARGET))
-	chmod +x $(PROJECT_BUILD_DIR)/_fakeroot.$(notdir $(BB_INITRAMFS_TARGET))
+	) > $(PROJECT_BUILD_DIR)/_fakeroot.$(BB_INITRAMFS_TARGET_F)
+	chmod +x $(PROJECT_BUILD_DIR)/_fakeroot.$(BB_INITRAMFS_TARGET_F)
 	$(STAGING_DIR)/usr/bin/fakeroot -- \
-		$(PROJECT_BUILD_DIR)/_fakeroot.$(notdir $(BB_INITRAMFS_TARGET))
-	rm -f $(PROJECT_BUILD_DIR)/_fakeroot.$(notdir $(BB_INITRAMFS_TARGET))
+		$(PROJECT_BUILD_DIR)/_fakeroot.$(BB_INITRAMFS_TARGET_F)
+	rm -f $(PROJECT_BUILD_DIR)/_fakeroot.$(BB_INITRAMFS_TARGET_F)
 	touch -c $@
 
 $(PROJECT_BUILD_DIR)/.initramfs_done: $(BR2_INITRAMFS_DIR)/bin/busybox \
@@ -140,9 +141,9 @@ busybox-initramfs-menuconfig: host-sed $(BUILD_DIR) busybox-source $(BUSYBOX_INI
 		-C $(BUSYBOX_INITRAMFS_DIR) menuconfig
 
 busybox-initramfs-clean:
+	-$(MAKE) -C $(BUSYBOX_INITRAMFS_DIR) clean
 	rm -f $(BUSYBOX_INITRAMFS_DIR)/busybox $(PROJECT_BUILD_DIR)/.initramfs_*
 	rm -rf $(BR2_INITRAMFS_DIR) $(BB_INITRAMFS_TARGET)
-	-$(MAKE) -C $(BUSYBOX_INITRAMFS_DIR) clean
 
 busybox-initramfs-dirclean:
 	rm -rf $(BUSYBOX_INITRAMFS_DIR) $(BR2_INITRAMFS_DIR) \
