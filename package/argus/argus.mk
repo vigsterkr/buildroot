0d179fd1cb6927e204e0025184bf032d83140ab8
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-04-01 09:05:48 +0200

    revert back to argus2

--- package/argus/argus.mk
+++ package/argus/argus.mk
@@ -4,8 +4,10 @@
 #
 #############################################################
 ARGUS_VERSION:=3.0.0.rc.34
+ARGUS_VERSION:=2.0.6.fixes.1
 ARGUS_SOURCE:=argus_$(ARGUS_VERSION).orig.tar.gz
 ARGUS_PATCH:=argus_$(ARGUS_VERSION)-1.diff.gz
+ARGUS_PATCH:=argus_$(ARGUS_VERSION)-13.diff.gz
 ARGUS_SITE:=$(BR2_DEBIAN_MIRROR)/debian/pool/main/a/argus/
 ARGUS_DIR:=$(BUILD_DIR)/argus-$(ARGUS_VERSION)
 ARGUS_CAT:=$(ZCAT)
@@ -20,7 +22,7 @@ $(DL_DIR)/$(ARGUS_PATCH):
 
 $(ARGUS_DIR)/.unpacked: $(DL_DIR)/$(ARGUS_SOURCE) $(DL_DIR)/$(ARGUS_PATCH)
 	$(ARGUS_CAT) $(DL_DIR)/$(ARGUS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	toolchain/patch-kernel.sh $(ARGUS_DIR) package/argus/ argus\*.patch
+	toolchain/patch-kernel.sh $(ARGUS_DIR) package/argus/ argus-$(ARGUS_VERSION)\*.patch
 ifneq ($(ARGUS_PATCH),)
 	(cd $(ARGUS_DIR) && $(ARGUS_CAT) $(DL_DIR)/$(ARGUS_PATCH) | patch -p1)
 	if [ -d $(ARGUS_DIR)/debian/patches ]; then \
@@ -32,19 +34,17 @@ endif
 
 $(ARGUS_DIR)/.configured: $(ARGUS_DIR)/.unpacked
 	(cd $(ARGUS_DIR); rm -rf config.cache; \
-		$(TARGET_CONFIGURE_OPTS) \
-		$(TARGET_CONFIGURE_ARGS) \
-		./configure \
-		--target=$(GNU_TARGET_NAME) \
-		--host=$(GNU_TARGET_NAME) \
-		--build=$(GNU_HOST_NAME) \
+		ac_cv_prog_V_LEX=flex \
+		ac_cv_lbl_flex_v24=yes \
+		$(AUTO_CONFIGURE_TARGET) \
 		--prefix=/usr \
 		$(DISABLE_LARGEFILE) \
+		--with-pcap=linux \
 	)
 	touch $@
 
 $(ARGUS_DIR)/$(ARGUS_BINARY): $(ARGUS_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(ARGUS_DIR)
+	$(MAKE) -C $(ARGUS_DIR)
 
 $(TARGET_DIR)/$(ARGUS_TARGET_BINARY): $(ARGUS_DIR)/$(ARGUS_BINARY)
 	$(INSTALL) -D $(ARGUS_DIR)/$(ARGUS_BINARY) $@
