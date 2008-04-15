e20562c209c0b306d745ddd155ef0893dde13111
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-04-15 13:54:00 +0200

    add gpm package
      for me to test mouse-support in qemu

---
+++ package/gpm/gpm.mk
@@ -0,0 +1,7 @@
+# gpm -- General Purpose Mouse
+#GPM_VERSION = 1.20.3pre6
+GPM_VERSION = 1.99.5
+GPM_SOURCE = gpm-$(GPM_VERSION).tar.bz2
+GPM_SITE = http://unix.schottelius.org/gpm/archives
+GPM_BISON_FLAGS = -y
+$(eval $(call AUTOTARGETS,package,gpm))
