b63474f40dbe9365e59859687fad59feefa2aa98
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-04-23 12:35:29 +0200

    bump version

--- package/gpm/gpm.mk
+++ package/gpm/gpm.mk
@@ -1,6 +1,6 @@
 # gpm -- General Purpose Mouse
 #GPM_VERSION = 1.20.3pre6
-GPM_VERSION = 1.99.5
+GPM_VERSION = 1.99.6
 GPM_SOURCE = gpm-$(GPM_VERSION).tar.bz2
 GPM_SITE = http://unix.schottelius.org/gpm/archives
 GPM_BISON_FLAGS = -y
