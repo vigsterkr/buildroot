00618083cb12b31a5b60fd2147218429b1a5bf56
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-06-07 23:55:29 +0200

    bump version

--- package/openssl/openssl.mk
+++ package/openssl/openssl.mk
@@ -5,23 +5,38 @@
 #############################################################
 
 # TARGETS
-OPENSSL_VERSION:=0.9.7m
+OPENSSL_VERSION:=0.9.8h
 OPENSSL_SITE:=http://www.openssl.org/source
 OPENSSL_SOURCE:=openssl-$(OPENSSL_VERSION).tar.gz
 OPENSSL_CAT:=$(ZCAT)
 OPENSSL_DIR:=$(BUILD_DIR)/openssl-$(OPENSSL_VERSION)
 
+#Usage: Configure [no-<cipher> ...] [enable-<cipher> ...] [-Dxxx] [-lxxx] [-Lxxx] [-fxxx] [-Kxxx] [no-hw-xxx|no-hw] [[no-]threads] [[no-]shared] [[no-]zlib|zlib-dynamic] [enable-montasm] [no-asm] [no-dso] [no-krb5] [386] [--prefix=DIR] [--openssldir=OPENSSLDIR] [--with-xxx[=vvv]] [--test-sanity] os/compiler[:flags]
+
 OPENSSL_TARGET_ARCH:=
 ifeq ($(BR2_i386),y)
 ifneq ($(ARCH),i386)
 OPENSSL_TARGET_ARCH:=i386-$(ARCH)
+OPENSSL_TARGET_ARCH:=generic32
 endif
 ifeq ($(ARCH),i686)
 OPENSSL_TARGET_ARCH:=i386-i686/cmov
+OPENSSL_TARGET_ARCH:=elf
 endif
 endif
+
+ifeq ($(BR2_alpha),y)
+OPENSSL_TARGET_ARCH:=linux-alpha-gcc
+endif
+
 ifeq ($(OPENSSL_TARGET_ARCH),)
 OPENSSL_TARGET_ARCH:=$(ARCH)
+endif
+
+ifeq ($(BR2_PTHREADS_NONE),y)
+OPENSSL_THREADS=no-threads
+else
+OPENSSL_THREADS=threads
 endif
 
 $(DL_DIR)/$(OPENSSL_SOURCE):
@@ -45,7 +60,8 @@ $(OPENSSL_DIR)/Makefile: $(OPENSSL_DIR)/.unpacked
 	PATH=$(TARGET_PATH) \
 	./Configure linux-$(OPENSSL_TARGET_ARCH) --prefix=/ \
 		--openssldir=/lib/ssl -L$(STAGING_DIR)/lib -ldl \
-		-I$(STAGING_DIR)/usr/include $(OPENSSL_OPTS) threads \
+		-I$(STAGING_DIR)/usr/include $(OPENSSL_OPTS) \
+		$(OPENSSL_THREADS) \
 		shared no-idea no-mdc2 no-rc5)
 
 # --openssldir=/usr/lib/ssl results in files in /usr/usr/lib...
@@ -57,28 +73,28 @@ $(OPENSSL_DIR)/apps/openssl: $(OPENSSL_DIR)/Makefile
 	$(MAKE1) CC=$(TARGET_CC) -C $(OPENSSL_DIR) do_linux-shared
 
 $(STAGING_DIR)/usr/lib/libcrypto.a: $(OPENSSL_DIR)/apps/openssl
-	$(MAKE) CC=$(TARGET_CC) INSTALL_PREFIX=$(STAGING_DIR)/usr -C $(OPENSSL_DIR) install
+	$(MAKE) CC=$(TARGET_CC) INSTALL_PREFIX=$(STAGING_DIR)/usr INSTALLTOP=$(STAGING_DIR)/usr/ -C $(OPENSSL_DIR) install
 	cp -fa $(OPENSSL_DIR)/libcrypto.so* $(STAGING_DIR)/usr/lib/
-	chmod a-x $(STAGING_DIR)/usr/lib/libcrypto.so.0.9.7
+	chmod a-x $(STAGING_DIR)/usr/lib/libcrypto.so.0.9.8
 	(cd $(STAGING_DIR)/usr/lib; \
-	 ln -fs libcrypto.so.0.9.7 libcrypto.so; \
-	 ln -fs libcrypto.so.0.9.7 libcrypto.so.0; \
+	 ln -fs libcrypto.so.0.9.8 libcrypto.so; \
+	 ln -fs libcrypto.so.0.9.8 libcrypto.so.0; \
 	)
 	cp -fa $(OPENSSL_DIR)/libssl.so* $(STAGING_DIR)/usr/lib/
-	chmod a-x $(STAGING_DIR)/usr/lib/libssl.so.0.9.7
+	chmod a-x $(STAGING_DIR)/usr/lib/libssl.so.0.9.8
 	(cd $(STAGING_DIR)/usr/lib; \
-	 ln -fs libssl.so.0.9.7 libssl.so; \
-	 ln -fs libssl.so.0.9.7 libssl.so.0; \
+	 ln -fs libssl.so.0.9.8 libssl.so; \
+	 ln -fs libssl.so.0.9.8 libssl.so.0; \
 	)
 	touch -c $@
 
-$(TARGET_DIR)/usr/lib/libcrypto.so.0.9.7: $(STAGING_DIR)/usr/lib/libcrypto.a
+$(TARGET_DIR)/usr/lib/libcrypto.so.0.9.8: $(STAGING_DIR)/usr/lib/libcrypto.a
 	mkdir -p $(TARGET_DIR)/usr/lib
 	cp -fa $(STAGING_DIR)/usr/lib/libcrypto.so* $(TARGET_DIR)/usr/lib/
 	cp -fa $(STAGING_DIR)/usr/lib/libssl.so* $(TARGET_DIR)/usr/lib/
 	#cp -fa $(STAGING_DIR)/bin/openssl $(TARGET_DIR)/bin/
-	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libssl.so.0.9.7
-	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libcrypto.so.0.9.7
+	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libssl.so.0.9.8
+	-$(STRIPCMD) $(STRIP_STRIP_UNNEEDED) $(TARGET_DIR)/usr/lib/libcrypto.so.0.9.8
 
 $(TARGET_DIR)/usr/lib/libssl.a: $(STAGING_DIR)/usr/lib/libcrypto.a
 	mkdir -p $(TARGET_DIR)/usr/include
@@ -89,7 +105,7 @@ $(TARGET_DIR)/usr/lib/libssl.a: $(STAGING_DIR)/usr/lib/libcrypto.a
 
 openssl-headers: $(TARGET_DIR)/usr/lib/libssl.a
 
-openssl: uclibc $(TARGET_DIR)/usr/lib/libcrypto.so.0.9.7
+openssl: uclibc $(TARGET_DIR)/usr/lib/libcrypto.so.0.9.8
 
 openssl-source: $(DL_DIR)/$(OPENSSL_SOURCE)
 
