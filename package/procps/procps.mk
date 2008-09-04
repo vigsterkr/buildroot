f42dd632988b89fb76dc0c950f8f48d1e60f5396
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-09-04 11:52:56 +0200

    bump version and depend on wchar

--- package/procps/procps.mk
+++ package/procps/procps.mk
@@ -3,7 +3,7 @@
 # procps
 #
 #############################################################
-PROCPS_VERSION:=3.2.5
+PROCPS_VERSION:=3.2.7
 PROCPS_SOURCE:=procps-$(PROCPS_VERSION).tar.gz
 PROCPS_SITE:=http://procps.sourceforge.net/
 PROCPS_DIR:=$(BUILD_DIR)/procps-$(PROCPS_VERSION)
@@ -16,16 +16,23 @@ $(DL_DIR)/$(PROCPS_SOURCE):
 $(PROCPS_DIR)/.source: $(DL_DIR)/$(PROCPS_SOURCE)
 	$(ZCAT) $(DL_DIR)/$(PROCPS_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(PROCPS_DIR) package/procps/ procps\*.patch
-	touch $(PROCPS_DIR)/.source
+	touch $@
 
 $(PROCPS_DIR)/$(PROCPS_BINARY): $(PROCPS_DIR)/.source
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(PROCPS_DIR)
+	$(TARGET_CONFIGURE_OPTS) $(TARGET_CONFIGURE_ENV) \
+		$(MAKE) -C $(PROCPS_DIR)
 
 $(TARGET_DIR)/$(PROCPS_TARGET_BINARY): $(PROCPS_DIR)/$(PROCPS_BINARY)
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) DESTDIR=$(TARGET_DIR) \
 		install='install -D' -C $(PROCPS_DIR) lib64=/lib \
 		ldconfig='/bin/true' install
-	rm -Rf $(TARGET_DIR)/usr/share/man
+ifneq ($(BR2_HAVE_MANPAGES),y)
+	rm -rf $(TARGET_DIR)/usr/man
+endif
+ifneq ($(BR2_HAVE_INFOPAGES),y)
+	rm -rf $(TARGET_DIR)/usr/info
+endif
+	$(STRIP) $(STRIP_STRIP_ALL) $@
 
 procps: uclibc ncurses $(TARGET_DIR)/$(PROCPS_TARGET_BINARY)
 
