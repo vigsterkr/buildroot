62a7650041f4ecb93b3a867d59060b5ceb4b3b31
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-09-26 17:31:30 +0200

    fix typo

--- package/ltp-testsuite/ltp-testsuite.mk
+++ package/ltp-testsuite/ltp-testsuite.mk
@@ -14,7 +14,6 @@ LTP_TESTSUITE_DIR:=$(LTP_TESTSUITE_ROOT)/ltp-full-$(LTP_TESTSUITE_VERSION)
 # Enable patches based upon different toolchain configuration options.
 #
 LTP_PATCHES:=ltp-testsuite-generate-needs-bash.patch \
-	     ltp-testsuite-sh-is-not-C-code.patch \
 	     ltp-testsuite.patch \
 	     ltp-testsuite.asm-page-include.patch \
 	     ltp-testsuite.obsolete-bsd-signal.patch \
@@ -26,25 +25,25 @@ endif
 ifeq ($(BR2_EXT_PTHREADS_NATIVE),y)
 LTP_PATCHES+=ltp-testsuite-enable-openposix-for-nptl.patch
 endif
-ifneq ($(BR2_INET_IPV6),y)
+ifeq ($(BR2_INET_IPV6),)
 LTP_PATCHES+=ltp-testsuite-disable-ipv6-tests.patch
 endif
 
-LTP_TESTSUITE_ENV:= \
-	UCLIBC_HAS_OBSOLETE_BSD_SIGNAL=$(BR2__UCLIBC_HAS_OBSOLETE_BSD_SIGNAL) \
-	UCLIBC_SUSV3_LEGACY=$(BR2__UCLIBC_SUSV3_LEGACY) \
-	UCLIBC_SV4_DEPRECATED=$(BR2__UCLIBC_SV4_DEPRECATED)
+LTP_TESTSUITE_ENV=\
+	UCLIBC_HAS_OBSOLETE_BSD_SIGNAL=$(BR2__UCLIBC_UCLIBC_HAS_OBSOLETE_BSD_SIGNAL) \
+	UCLIBC_SUSV3_LEGACY=$(BR2__UCLIBC_UCLIBC_SUSV3_LEGACY) \
+	UCLIBC_SV4_DEPRECATED=$(BR2__UCLIBC_UCLIBC_SV4_DEPRECATED)
 
 $(DL_DIR)/$(LTP_TESTSUITE_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(LTP_TESTSUITE_SITE)/$(LTP_TESTSUITE_SOURCE)
 
-$(LTP_TESTSUITE_DIR)/Makefile: $(DL_DIR)/$(LTP_TESTSUITE_SOURCE)
+$(LTP_TESTSUITE_DIR)/.configured: $(DL_DIR)/$(LTP_TESTSUITE_SOURCE)
 	mkdir -p $(LTP_TESTSUITE_ROOT)
 	$(LTP_TESTSUITE_CAT) $(DL_DIR)/$(LTP_TESTSUITE_SOURCE) | tar -C $(LTP_TESTSUITE_ROOT) $(TAR_OPTIONS) -
 	toolchain/patch-kernel.sh $(LTP_TESTSUITE_DIR) package/ltp-testsuite/ $(LTP_PATCHES)
-	touch -c $@
+	touch $@
 
-$(LTP_TESTSUITE_DIR)/.compiled: $(LTP_TESTSUITE_DIR)/Makefile
+$(LTP_TESTSUITE_DIR)/.compiled: $(LTP_TESTSUITE_DIR)/.configured
 	$(MAKE1) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILER=$(TARGET_CROSS) \
 		$(LTP_TESTSUITE_ENV) \
 		-C $(LTP_TESTSUITE_DIR) all
@@ -52,8 +51,10 @@ $(LTP_TESTSUITE_DIR)/.compiled: $(LTP_TESTSUITE_DIR)/Makefile
 
 $(LTP_TESTSUITE_DIR)/.installed: $(LTP_TESTSUITE_DIR)/.compiled
 	# Use fakeroot to pretend to do 'make install' as root
-	echo '$(MAKE1) $(TARGET_CONFIGURE_OPTS) CROSS_COMPILER=$(TARGET_CROSS) ' \
-			'-C $(LTP_TESTSUITE_DIR) install' \
+	echo '$(MAKE1) $(TARGET_CONFIGURE_OPTS) \
+			CROSS_COMPILER=$(TARGET_CROSS) \
+			$(LTP_TESTSUITE_ENV) \
+			-C $(LTP_TESTSUITE_DIR) install' \
 			> $(PROJECT_BUILD_DIR)/.fakeroot.ltp
 	touch $@
 
@@ -63,6 +64,7 @@ ltp-testsuite-source: $(DL_DIR)/$(LTP_TESTSUITE_SOURCE)
 
 ltp-testsuite-clean:
 	-$(MAKE) -C $(LTP_TESTSUITE_DIR) clean
+	rm -f $(LTP_TESTSUITE_DIR)/.compiled
 
 ltp-testsuite-dirclean:
 	rm -rf $(LTP_TESTSUITE_DIR)
