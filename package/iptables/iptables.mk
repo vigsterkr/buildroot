cd147a577851f2528656e92e7bf656184bcf25ed
Bernhard Fischer <rep.dot.nop@gmail.com>
2008-10-13 19:30:08 +0200

    bump version

--- package/iptables/iptables.mk
+++ package/iptables/iptables.mk
@@ -3,64 +3,21 @@
 # iptables
 #
 #############################################################
-IPTABLES_VERSION:=1.4.0
-IPTABLES_SOURCE_URL:=http://ftp.netfilter.org/pub/iptables
-IPTABLES_SOURCE:=iptables-$(IPTABLES_VERSION).tar.bz2
-IPTABLES_CAT:=$(BZCAT)
-IPTABLES_BUILD_DIR:=$(BUILD_DIR)/iptables-$(IPTABLES_VERSION)
+IPTABLES_VERSION = 20081006
+IPTABLES_SITE = http://ftp.netfilter.org/pub/iptables/snapshot
 
-$(DL_DIR)/$(IPTABLES_SOURCE):
-	 $(WGET) -P $(DL_DIR) $(IPTABLES_SOURCE_URL)/$(IPTABLES_SOURCE)
+IPTABLES_VERSION = 1.4.2
+IPTABLES_SITE = http://ftp.netfilter.org/pub/iptables
 
-$(IPTABLES_BUILD_DIR)/.unpacked: $(DL_DIR)/$(IPTABLES_SOURCE)
-	$(IPTABLES_CAT) $(DL_DIR)/$(IPTABLES_SOURCE) | tar -C $(BUILD_DIR) $(TAR_OPTIONS) -
-	touch $@
+IPTABLES_SOURCE = iptables-$(IPTABLES_VERSION).tar.bz2
 
-$(IPTABLES_BUILD_DIR)/.configured: $(IPTABLES_BUILD_DIR)/.unpacked
-	# Allow patches. Needed for openwrt for instance.
-	toolchain/patch-kernel.sh $(IPTABLES_BUILD_DIR) package/iptables/ iptables\*.patch
-	#
-	$(SED) "s;\[ -f /usr/include/netinet/ip6.h \];grep -q '__UCLIBC_HAS_IPV6__ 1' \
-		$(STAGING_DIR)/usr/include/bits/uClibc_config.h;" $(IPTABLES_BUILD_DIR)/Makefile
-	touch $@
+IPTABLES_CONF_OPT = --libexecdir=/usr/lib
+IPTABLES_INSTALL_TARGET = YES
+IPTABLES_INSTALL_TARGET_OPT = DESTDIR=$(TARGET_DIR) install
 
-$(IPTABLES_BUILD_DIR)/iptables: $(IPTABLES_BUILD_DIR)/.configured
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(IPTABLES_BUILD_DIR) \
-		KERNEL_DIR=$(LINUX_HEADERS_DIR) \
-		COPT_FLAGS="$(TARGET_CFLAGS)" \
-		PREFIX=/usr \
-		INCDIR="\$$(PREFIX)/include" \
-		MANDIR="\$$(PREFIX)/share/man"
+#IPTABLES_AUTORECONF = YES
+#IPTABLES_DEPENDENCIES =
+IPTABLES_CONFIGURE_OPT = --with-kernel=$(LINUX_HEADERS_DIR)
+#IPTABLES_MAKE_OPT = GLIB_GENMARSHAL=/usr/bin/glib-genmarshal GLIB_MKENUMS=/usr/bin
 
-$(TARGET_DIR)/usr/sbin/iptables: $(IPTABLES_BUILD_DIR)/iptables
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(IPTABLES_BUILD_DIR) \
-		KERNEL_DIR=$(LINUX_HEADERS_DIR) \
-		COPT_FLAGS="$(TARGET_CFLAGS)" \
-		PREFIX=/usr \
-		INCDIR="\$$(PREFIX)/include" \
-		MANDIR="\$$(PREFIX)/share/man" \
-		DESTDIR=$(TARGET_DIR) install
-	$(STRIPCMD) $(TARGET_DIR)/usr/sbin/iptables*
-	$(STRIPCMD) $(TARGET_DIR)/usr/lib/iptables/*.so
-ifneq ($(BR2_HAVE_MANPAGES),y)
-	rm -rf $(TARGET_DIR)/usr/share/man
-endif
-
-iptables: $(TARGET_DIR)/usr/sbin/iptables
-
-iptables-source: $(DL_DIR)/$(IPTABLES_SOURCE)
-
-iptables-clean:
-	-$(MAKE1) -C $(IPTABLES_BUILD_DIR) clean
-	rm -rf $(TARGET_DIR)/usr/sbin/iptables* $(TARGET_DIR)/usr/lib/iptables
-
-iptables-dirclean:
-	rm -rf $(IPTABLES_BUILD_DIR)
-#############################################################
-#
-# Toplevel Makefile options
-#
-#############################################################
-ifeq ($(BR2_PACKAGE_IPTABLES),y)
-TARGETS+=iptables
-endif
+$(eval $(call AUTOTARGETS,package,iptables))
