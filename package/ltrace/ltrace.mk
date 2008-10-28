d892c8b1b04789e3a1c2566be260f93ae6bad327
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2008-10-28 13:05:26 +0100

    bump version
      and fix compilation for powerpc

--- package/ltrace/ltrace.mk
+++ package/ltrace/ltrace.mk
@@ -5,15 +5,15 @@
 #############################################################
 LTRACE_VERSION=0.5
 LTRACE_SOURCE=ltrace_$(LTRACE_VERSION).orig.tar.gz
-LTRACE_PATCH=ltrace_$(LTRACE_VERSION)-3.diff.gz
+LTRACE_PATCH=ltrace_$(LTRACE_VERSION)-3.1.diff.gz
 LTRACE_SITE=$(BR2_DEBIAN_MIRROR)/debian/pool/main/l/ltrace
 LTRACE_DIR=$(BUILD_DIR)/ltrace-$(LTRACE_VERSION)
 LTRACE_BINARY=ltrace
 LTRACE_TARGET_BINARY=usr/bin/ltrace
 
 LTRACE_ARCH:=$(KERNEL_ARCH)
-ifeq ("$(strip $(ARCH))","armeb")
-LTRACE_ARCH:=arm
+ifeq ("$(strip $(LTRACE_ARCH))","powerpc")
+LTRACE_ARCH:=ppc
 endif
 
 $(DL_DIR)/$(LTRACE_SOURCE):
@@ -37,12 +37,13 @@ endif
 	$(CONFIG_UPDATE) $(@D)
 	chmod +x $(LTRACE_DIR)/configure
 	ln -sf linux-gnu $(LTRACE_DIR)/sysdeps/linux-uclibc
+	$(SED) 's/@\(DEFS\)@/@\1@ $$(\1)/' $(LTRACE_DIR)/Makefile.in
 	touch $@
 
 $(LTRACE_DIR)/.configured: $(LTRACE_DIR)/.patched
 	(cd $(LTRACE_DIR); rm -rf config.cache; \
+		ac_cv_header_gelf_h=yes \
 		$(AUTO_CONFIGURE_TARGET) \
-		CPPFLAGS="-DUSE_DEMANGLE" \
 		--prefix=/usr \
 		--sysconfdir=/etc \
 		$(DISABLE_LARGEFILE) \
@@ -51,6 +52,7 @@ $(LTRACE_DIR)/.configured: $(LTRACE_DIR)/.patched
 
 $(LTRACE_DIR)/$(LTRACE_BINARY): $(LTRACE_DIR)/.configured
 	$(MAKE) ARCH=$(LTRACE_ARCH) \
+		DEFS="-DUSE_DEMANGLE" \
 		-C $(LTRACE_DIR)
 
 $(TARGET_DIR)/$(LTRACE_TARGET_BINARY): $(LTRACE_DIR)/$(LTRACE_BINARY)
