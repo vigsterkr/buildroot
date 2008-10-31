72f8c664d21e60a6b972b4eab025ca8b8b0d6521
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2008-10-31 17:57:27 +0100

    bump version

--- target/powerpc/yaboot/yaboot.mk
+++ target/powerpc/yaboot/yaboot.mk
@@ -5,27 +5,49 @@ ifeq ($(ARCH),powerpc)
 # yaboot
 #
 #############################################################
-
-YABOOT_SOURCE:=yaboot-1.3.13.tar.gz
-YABOOT_SITE:=http://penguinppc.org/bootloaders/yaboot
-YABOOT_DIR:=$(BUILD_DIR)/yaboot-1.3.13
+#YABOOT_SOURCE:=yaboot-1.3.13.tar.gz
+#YABOOT_SITE:=http://penguinppc.org/bootloaders/yaboot
+YABOOT_VERSION:=1.3.14
+YABOOT_SOURCE:=yaboot-$(YABOOT_VERSION).tar.gz
+YABOOT_SITE:=http://yaboot.ozlabs.org/releases
+YABOOT_DIR:=$(BUILD_DIR)/yaboot-$(YABOOT_VERSION)
 
 $(DL_DIR)/$(YABOOT_SOURCE):
 	 $(WGET) -P $(DL_DIR) $(YABOOT_SITE)/$(YABOOT_SOURCE)
 
-yaboot-source: $(DL_DIR)/$(YABOOT_SOURCE)
+$(YABOOT_DIR)/.unpacked: $(DL_DIR)/$(YABOOT_SOURCE)
+	$(ZCAT) $(DL_DIR)/$(YABOOT_SOURCE) | tar -C $(BUILD_DIR) -xf -
+	(cd $(@D) && test -f man.patch && patch -p0 < man.patch)
+	touch $@
 
-$(YABOOT_DIR)/Makefile: $(DL_DIR)/$(YABOOT_SOURCE)
-	$(ZCAT) $(DL_DIR)/$(YABOOT_SOURCE) | tar -C $(BUILD_DIR) -xvf -
-	touch -c $(YABOOT_DIR)/Makefile
+$(YABOOT_DIR)/.configured: $(YABOOT_DIR)/.unpacked $(wildcard $(BR2_DEPENDS_DIR)/br2/yaboot/have/*.h)$(wildcard $(BR2_DEPENDS_DIR)/br2/yaboot/fs/*.h)
+	(echo 'CONFIG_COLOR_TEXT  :=$(BR2_YABOOT_HAVE_COLORTEXT)'; \
+	 echo 'CONFIG_SET_COLORMAP:=$(BR2_YABOOT_HAVE_COLORMAP)'; \
+	 echo 'USE_MD5_PASSWORDS  :=$(BR2_YABOOT_HAVE_MD5)'; \
+	 echo 'CONFIG_FS_XFS      :=$(BR2_YABOOT_FS_XFS)'; \
+	 echo 'CONFIG_FS_REISERFS :=$(BR2_YABOOT_FS_REISERFS)'; \
+	) > $(YABOOT_DIR)/Config
+	touch $@
 
-$(YABOOT_DIR)/second/yaboot: $(YABOOT_DIR)/Makefile
-	$(MAKE) -C $(YABOOT_DIR) CROSS=$(TARGET_CROSS)
+YABOOT_DIRS=\
+		PREFIX=usr \
+		MANDIR=share \
+		ROOT="$(TARGET_DIR)"
+
+$(YABOOT_DIR)/second/yaboot: $(YABOOT_DIR)/.configured e2fsprogs-libs
+	$(MAKE) -C $(YABOOT_DIR) CROSS=$(TARGET_CROSS) \
+		CFLAGS="$(TARGET_CFLAGS)" \
+		LD="$(TARGET_LD) $(TARGET_LD_FLAGS) $(TARGET_LDFLAGS)" \
+		HOSTCC="$(HOSTCC)" HOSTCFLAGS="$(HOST_CFLAGS)" \
+		$(YABOOT_DIRS)
 	touch -c $(YABOOT_DIR)/second/yaboot
 
 yaboot: $(YABOOT_DIR)/second/yaboot
 
+yaboot-source: $(DL_DIR)/$(YABOOT_SOURCE)
+
 yaboot-clean:
+	$(MAKE) -C $(YABOOT_DIR) $(YABOOT_DIRS) uninstall
 	$(MAKE) -C $(YABOOT_DIR) clean
 
 yaboot-dirclean:
